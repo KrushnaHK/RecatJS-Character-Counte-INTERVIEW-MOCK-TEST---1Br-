5f2903fa542df12e437739ee50e36ab313262897
KrushnaHK <krushnahandibag02@gmail.com.com>
2024-02-03 07:09:46 +0000

    Character Counter

---
+++ src/components/CharacterCounter/index.js
@@ -0,0 +1,89 @@
+import {Component} from 'react'
+import {v4} from 'uuid'
+import UserInput from '../UserInput'
+
+import {
+  BgContainer,
+  LeftPanel,
+  InfoCardContainer,
+  Info,
+  UserInputList,
+  RightPanel,
+  CounterHeading,
+  AddInputContainer,
+  Input,
+  AddInputButton,
+  EmptyImage,
+} from './styledComponents'
+
+class CharacterCounter extends Component {
+  state = {
+    userInputList: [],
+    userInput: '',
+  }
+
+  onChangeUserInput = event => {
+    this.setState({userInput: event.target.value})
+  }
+
+  onAddUserInput = event => {
+    event.preventDefault()
+    const {userInput} = this.state
+
+    const newUserInput = {
+      id: v4(),
+      userEnteredText: userInput,
+      textLength: userInput.length,
+    }
+
+    this.setState(prevState => ({
+      userInputList: [...prevState.userInputList, newUserInput],
+      userInput: '',
+    }))
+  }
+
+  renderUserInputs = () => {
+    const {userInputList} = this.state
+    return userInputList.length === 0 ? (
+      <EmptyImage
+        src="https://assets.ccbp.in/frontend/react-js/no-user-inputs-img.png"
+        alt="no user inputs"
+      />
+    ) : (
+      userInputList.map(eachItem => (
+        <UserInput key={eachItem.id} userInputDetails={eachItem} />
+      ))
+    )
+  }
+
+  render() {
+    const {userInput} = this.state
+    return (
+      <BgContainer>
+        <LeftPanel>
+          <InfoCardContainer>
+            <Info>
+              Count the characters like a <br />
+              Boss...
+            </Info>
+          </InfoCardContainer>
+          <UserInputList>{this.renderUserInputs()}</UserInputList>
+        </LeftPanel>
+        <RightPanel>
+          <CounterHeading>Character Counter</CounterHeading>
+          <AddInputContainer onSubmit={this.onAddUserInput}>
+            <Input
+              type="text"
+              value={userInput}
+              onChange={this.onChangeUserInput}
+              placeholder="Enter the characters here"
+            />
+            <AddInputButton>Add</AddInputButton>
+          </AddInputContainer>
+        </RightPanel>
+      </BgContainer>
+    )
+  }
+}
+
+export default CharacterCounter
